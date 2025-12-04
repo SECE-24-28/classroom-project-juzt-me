12f361b021e6a49658bf1cbf9d07cdbcb51e4d20
pavithra_M <m.pavithra2024cse@sece.ac.in>
2025-12-04 08:45:09 +0530

    This is new website

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,29 @@
+/*import "./App.css";
+import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
+
+function App() {
+  return (
+    <div>
+      {<HelloWorldComponent /> }
+      {<FlexBoxComponent /> }
+      <LeadersTomorrowComponent />
+      
+    </div>
+  );
+}
+
+export default App;
+
+*/
+import React from "react";
+import RectangleBox from "./rectangle-box/rectangle-box-component";
+
+function App() {
+  return (
+    <>
+      <RectangleBox />
+    </>
+  );
+}
+
+export default App;
